f3eec6d848ea4c0b00c57de6c525f70c0d186d31
Carlos Precioso <511681+cprecioso@users.noreply.github.com>
2022-03-03 02:59:34 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import path from "path";
+import type { Plugin } from "rollup";
+
+const utilsFile = require.resolve("./utils");
+
+const wasm = (): Plugin => ({
+  name: "wasm",
+  async transform(code, id) {
+    if (path.extname(id) !== ".wasm") return null;
+
+    this.addWatchFile(id);
+
+    const wasmReferenceId = this.emitFile({
+      type: "asset",
+      fileName: path.basename(id),
+      source: code,
+    });
+
+    return {
+      code: `
+import { instantiate } from "${utilsFile}";
+const mod = await instantiate(import.meta.ROLLUP_FILE_URL_${wasmReferenceId});
+export const __synthetic__ = mod.instance.exports;
+`,
+      syntheticNamedExports: "__synthetic__",
+      moduleSideEffects: false,
+    };
+  },
+});
+
+export default wasm;
